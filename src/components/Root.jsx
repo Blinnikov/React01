7a979510ce29a9300c2dc99dd9f4fef889e7f59a
Igor Blinnikov <igor.blinnikov@gmail.com>
2016-12-18 22:51:20 +0300

    feat(init): move AppContainer out of Root element

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-// AppContainer is a necessary wrapper component for HMR
-import { AppContainer } from 'react-hot-loader';
+
 import { Provider } from 'react-redux';
 import App from '../containers/App'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <AppContainer>
-      <App />
-    </AppContainer>
+    <App />
   </Provider>
 );
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './configureStore';
+// AppContainer is a necessary wrapper component for HMR
+import { AppContainer } from 'react-hot-loader';
 import Root from './components/Root';
 
 const store = configureStore();
 
 const render = () => {
   ReactDOM.render(
-    <Root store={store} />,
+    <AppContainer>
+      <Root store={store} />
+    </AppContainer>,
     document.getElementById('root')
   );
 };
@@ -17,6 +21,5 @@ render();
 
 // Hot Module Replacement API
 if (module.hot) {
-  // module.hot.accept('./containers/App', render);
   module.hot.accept('./components/Root', render);
 }
