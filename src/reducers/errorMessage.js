7826317e4632efeb7a0a31cfd690a8b2f9f5a960
Igor Blinnikov <igor.blinnikov@gmail.com>
2016-12-27 01:33:34 +0300

    fix(error message): add reducer to root reducer
    - export error message reducer
    - read error code and format message

--- src/reducers/errorMessage.js
+++ src/reducers/errorMessage.js
@@ -1,4 +1,4 @@
-const errorMessage = (state = null, action) {
+const errorMessage = (state = null, action) => {
   switch (action.type) {
     case 'GET_PHOTOS_REQUEST':
     case 'GET_PHOTOS_SUCCESS':
@@ -7,8 +7,23 @@ const errorMessage = (state = null, action) {
       return null;
     case 'GET_PHOTOS_FAILURE':
     case 'USER_LOGIN_FAILURE':
-      return action.message;
+      console.log('FAILURE REDUCER', action);
+      return `${action.message}. Error: ${getMessageFromCode(action.code)}`;
     default:
       return state;
   }
 }
+
+const getMessageFromCode = (code) => {
+  if (code == 15) {
+    return "Access denied.";
+  }
+
+  if (code === -100) {
+    return "Unknown."
+  }
+
+  return "";
+}
+
+export default errorMessage;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import userInfo from './userInfo';
 import layout from './layout';
 import loading from './loading';
 import authenticated from './authenticated';
+import errorMessage from './errorMessage';
 
 const rootReducer = combineReducers({
   userInfo,
   layout,
   loading,
-  authenticated
+  authenticated,
+  errorMessage
 })
 
 export default rootReducer;
