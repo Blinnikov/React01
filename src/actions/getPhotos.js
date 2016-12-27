188c8ce7979010708c7d81eef590a2f2df85f95f
Igor Blinnikov <igor.blinnikov@gmail.com>
2016-12-28 00:03:34 +0300

    feat(photo grid): add year filter

--- src/actions/getPhotos.js
+++ src/actions/getPhotos.js
@@ -1,3 +1,6 @@
+const count = 200;
+const minYear = 2014;
+
 const getPhotos = (year) => (dispatch, /* getState */) => {
   // TODO: Add isFetching check
 
@@ -6,30 +9,79 @@ const getPhotos = (year) => (dispatch, /* getState */) => {
     year
   });
 
-  // TODO: Add year filter
-  loadPhotosFromVk(0, 200, year).then(
-    photos => {
+
+  loadAllPhotosFromVkByYear(year).then(
+    photos =>
       dispatch({
         type: 'GET_PHOTOS_SUCCESS',
         year,
         photos
-      });
-    },
-    error => {
-      return dispatch({
+      }),
+    error =>
+      dispatch({
         type: 'GET_PHOTOS_FAILURE',
         year,
         code: error.error_code,
         message: error.error_msg
-      });
+      })
+  );
+};
+
+const loadAllPhotosFromVkByYear = (year) => {
+  const result = [];
+
+  return new Promise(
+    (resolve, reject) => {
+      loadAllPhotosFromVk().then(
+        photos => {
+          for (let p of photos) {
+            if (getYearFromPhoto(p) > year) {
+              continue;
+            }
+
+            if (getYearFromPhoto(p) < year) {
+              break;
+            }
+
+            result.push(p);
+          }
+
+          resolve(result);
+        },
+        error => {
+          reject(error);
+        }
+      );
     }
   );
 };
 
-const loadPhotosFromVk = (offset, count, year) => {
+const loadAllPhotosFromVk = (result = [], offset = 0) => {
   return new Promise(
     (resolve, reject) => {
-      VK.Api.call('photos.getAll', { extended: 1, v: 5.60 }, r => {
+      loadPhotosFromVk(offset, count).then(
+        photos => {
+          result = result.concat(photos);
+          const lastPhotoYear = getYearFromPhoto(photos[photos.length - 1]);
+          if (lastPhotoYear < minYear) {
+            resolve(result);
+          } else {
+            offset += count;
+            resolve(loadAllPhotosFromVk(result, offset));
+          }
+        },
+        error => {
+          reject(error);
+        }
+      )
+    }
+  );
+};
+
+const loadPhotosFromVk = (offset, count) => {
+  return new Promise(
+    (resolve, reject) => {
+      VK.Api.call('photos.getAll', { extended: 1, offset, count, v: 5.60 }, r => {
         if (r.response) {
           resolve(r.response.items);
         } else if(r.error) {
@@ -41,5 +93,10 @@ const loadPhotosFromVk = (offset, count, year) => {
     }
   );
 };
+
+const getYearFromPhoto = (photo) => {
+  const unixDate = photo.date;
+  return (new Date(unixDate * 1000)).getFullYear();
+}
 
 export default getPhotos;
